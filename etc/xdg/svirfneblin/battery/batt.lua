beee4299d5b6d8139e86c047257a4b04251f8023
dyngar <dyngar@dyngar.local>
2015-11-12 23:43:17 -0500

    switched to the closured version and modified it for the alienware m11x battery

--- etc/xdg/svirfneblin/battery/batt.lua
+++ etc/xdg/svirfneblin/battery/batt.lua
@@ -1,54 +1,82 @@
-function batteryInfo(adapter)
-   local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")    
-   local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
-   local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
-   if fcur and fcap and fsta then
-        local cur = fcur:read()
-        local cap = fcap:read()
-        local sta = fsta:read()
-        local battery = math.floor(cur * 100 / cap)
-        if sta:match("Charging") then
-            dir = "^"
-        elseif sta:match("Discharging") then
-            dir = "v"
-        else
-            dir = ""
-        end
-       fcur:close()
-       fcap:close()
-       fsta:close()
-       return {battery,dir}
-   end
-end
+local io = io
+local math = math
+local naughty = naughty
+local beautiful = beautiful
+local tonumber = tonumber
+local tostring = tostring
+local print = print
+local pairs = pairs
 
-function batteryShow(adapter)
-    local batInfos =  batteryInfo(adapter)
-    if batInfos then
-        local battery = batInfos[1]
-        local dir = batInfos[2]
-        infos = " " .. dir .. battery .. "% " 
+module("battery")
+
+local limits = {{25, 5},
+          {12, 3},
+          { 7, 1},
+            {0}}
+
+function get_bat_state (adapter)
+    local fcur = io.open("/sys/class/power_supply/"..adapter.."/energy_now")
+    local fcap = io.open("/sys/class/power_supply/"..adapter.."/energy_full")
+    local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
+    local cur = fcur:read()
+    local cap = fcap:read()
+    local sta = fsta:read()
+    fcur:close()
+    fcap:close()
+    fsta:close()
+    local battery = math.floor(cur * 100 / cap)
+    if sta:match("Charging") then
+        dir = 1
+    elseif sta:match("Discharging") then
+        dir = -1
     else
-       infos = "absente"
+        dir = 0
+        battery = ""
     end
-      naughty.notify({title = "Batterie",text = infos})
+    return battery, dir
 end
 
-function batteryCheck(adapter)
-    local batInfos = batteryInfo(adapter)
-    if batInfos then
-        local battery = batInfos[1]
-        local dir = batInfos[2]
-        if dir:match("v") and tonumber(battery) < 10 then
-               naughty.notify({ preset = naughty.config.presets.critical,
-                                title = "Batterie Low",
-                                text = " ".. battery .. "% left",
-                                timeout = 30,
-                                font = "Liberation 11", })
-       end
-       infos = " " .. dir .. battery .. "% "
-   else
-       infos = "A/C"
-   end
-   battery_widget.text = infos
+function getnextlim (num)
+    for ind, pair in pairs(limits) do
+        lim = pair[1]; step = pair[2]; nextlim = limits[ind+1][1] or 0
+        if num > nextlim then
+            repeat
+                lim = lim - step
+            until num > lim
+            if lim < nextlim then
+                lim = nextlim
+            end
+            return lim
+        end
+    end
 end
 
+
+function batclosure (adapter)
+    local nextlim = limits[1][1]
+    return function ()
+        local prefix = "⚡"
+        local battery, dir = get_bat_state(adapter)
+        if dir == -1 then
+            dirsign = "↓"
+            prefix = "Bat:"
+            if battery <= nextlim then
+                naughty.notify({title = "⚡ Beware! ⚡",
+                            text = "Battery charge is low ( ⚡ "..battery.."%)!",
+                            timeout = 7,
+                            position = "bottom_right",
+                            fg = beautiful.fg_focus,
+                            bg = beautiful.bg_focus
+                            })
+                nextlim = getnextlim(battery)
+            end
+        elseif dir == 1 then
+            dirsign = "↑"
+            nextlim = limits[1][1]
+        else
+            dirsign = ""
+        end
+        if dir ~= 0 then battery = battery.."%" end
+        return " "..prefix.." "..dirsign..battery..dirsign.." "
+    end
+end
