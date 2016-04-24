058b42e58316488f8e8cdbd064b4587bdc970b9b
idk <idk@idk.local>
2016-04-24 15:27:50 -0400

    Sun Apr 24 15:27:50 EDT 2016

--- debian.sh
+++ debian.sh
@@ -8,7 +8,8 @@ DEBFOLDER=svirfneblin-battery-widget
 
 DEBVERSION=$(date +%Y%m%d)
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $TOME
 
 git pull origin master
 
@@ -20,7 +21,7 @@ rm -rf $DEBFOLDERNAME
 mkdir $DEBFOLDERNAME
 
 # Copy your script to the source dir
-cp -R $SOURCEBINPATH/ $DEBFOLDERNAME/
+cp -R $TOME $DEBFOLDERNAME/
 cd $DEBFOLDERNAME
 
 pwd
